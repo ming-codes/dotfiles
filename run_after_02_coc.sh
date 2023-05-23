48dd2787783dec868f67619ae17398560a2180a2
Ming Liu <ming.codes@gmail.com>
2023-05-23 08:51:34 -0700

    fix fix fix

--- run_after_02_coc.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-cd .local/share/nvim/site/pack/plugins/start/coc.nvim
-npx yarn --frozen-lockfile
--- run_once_after_nvim.sh
+++ run_once_after_nvim.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env sh
 
-nvim --headless "+call firenvim#install(0) | q"
-nvim --headless "+TSUpdate | q"
