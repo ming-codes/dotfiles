dd6710af9dee1f355030fa0ba77ffc67232f2835
Ming Liu <ming-codes@users.noreply.github.com>
2024-12-03 09:40:41 -0800

    Update run_once_before_02_encryption.sh

--- run_once_before_02_encryption.sh
+++ run_once_before_02_encryption.sh
@@ -7,6 +7,7 @@ brew "gpg"
 EOF
 
 # FIXME gpg not in $PATH
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 #gpg --decrypt $CHEZMOI_WORKING_TREE/.encryption/gpg.public | gpg --import
 gpg --decrypt $CHEZMOI_WORKING_TREE/.encryption/gpg.secret | gpg --import
