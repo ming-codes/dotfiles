143d3094512d49b8b98ed8554a3639bbebacc2ad
Ming Liu <ming.codes@gmail.com>
2026-03-25 15:09:33 -0700

    wip

--- run_once_before_01_setup.sh
+++ run_once_before_01_setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" # Install Homebrew
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" # Install Homebrew
 
-curl -fsSL https://opencode.ai/install | bash # Install OpenCode
-curl -fsSL https://bun.sh/install | bash      # Install Bun
+#curl -fsSL https://opencode.ai/install | bash # Install OpenCode
+
+# brew tap oven-sh/bun
+# brew install bun
+#brew "anomalyco/tap/opencode"
