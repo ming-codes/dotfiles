dc99115fc2f34676294d41cce388cc5d8b40132b
Ming Liu <ming.codes@gmail.com>
2026-04-12 11:27:21 -0700

    wip

--- dot_config/visidata/config.py
+++ dot_config/visidata/config.py
@@ -1,13 +1,102 @@
-options.color_default = "white on black"
-options.color_key_col = "bold cyan"
-options.color_selected_row = "215 yellow"
-options.color_current_row = "reverse"
-options.color_current_col = "bold on 232"
-options.color_default_hdr = "bold white on black"
-options.color_bottom_hdr = "underline white on black"
-options.color_column_sep = "white on black"
-options.color_error = "bold 202"
-options.color_warning = "bold 166"
-options.color_status = "bold on 238"
-options.color_menu = "bold white on black"
+# Define colors as variables
+BG = "#eeeeee"
+ACCENT = "#005f87"
+ACCENT_LIGHT = "#0087af"
+
 options.undo = True
+
+color_default = "1 on 2"
+
+# Menu Bar (Top)
+options.color_menu = "bold 0 on 6"  # top menu bar
+options.color_menu_active = "6 on 0"
+# options.color_menu_help = "2 on 3"  # This is the float help
+# options.color_menu_spec = "1 on 2" # This is the special "open" highlight
+
+# Tabs (Bottom)
+# options.color_status = "bold 0 on 1"
+options.color_top_status = "0 on red"
+options.color_active_status = "0 on 15"
+# options.color_inactive_status = "0 on 3"
+# options.disp_status_sep = "$$$"
+# options.disp_status_fmt = "{sheet.sheetlist}[:white on 15]"  # This controls EVERYTHING
+options.disp_status_fmt = "{sheet.sheetlist}"
+
+# Cursor
+options.color_current_row = "7 on 253"
+options.color_current_col = "bold 7 on 250"
+
+# options.color_default = "white on #eeeeee"
+# options.color_key_col = "bold #0087af"
+# options.color_selected_row = "black on #eeeeee"
+# options.color_default_hdr = "bold 0 on 3" # hdr = header?
+# options.color_bottom_hdr = "underline white on 4"
+# options.color_column_sep = "white on #eeeeee"
+# options.color_error = "bold #d75f00"
+# options.color_warning = "bold #ff8700"
+# options.color_top_status = "bold 0 on 6"
+# options.color_status = "bold 0 on 6"
+
+# options.color_default_hdr = ("bold 0 on 15",)
+# options.color_bottom_hdr = ("underline 0 on 15",)
+# options.color_current_row = ("reverse",)
+# options.color_current_col = ("bold",)
+# options.color_current_hdr = "0 on 2"
+# options.color_column_sep = ("7 on 15",)
+# options.color_key_col = ("26 blue",)
+# options.color_edit_cell = ("26 blue",)
+# options.color_selected_row = ("0 on 111",)
+# options.color_note_row = ("178 yellow",)
+# options.color_note_type = ("26 blue",)
+# options.color_warning = ("178 yellow",)
+# options.color_error = ("160 red",)
+# options.color_working = ("bold 36 green",)
+# options.color_add_pending = ("36 green",)
+# options.color_change_pending = ("reverse 178 yellow",)
+# options.color_delete_pending = ("160 red",)
+
+# | Option                        | Description                                       |
+# | ----------------------------- | ------------------------------------------------- |
+# | disp_float_fmt                | Default format string for float values            |
+# | disp_int_fmt                  | Default format string for integer values          |
+# | disp_date_fmt                 | Default format string for date values             |
+# | disp_currency_fmt             | Default format string for currency values         |
+# | disp_menu_boxchars            | Box characters for menus                          |
+# | disp_menu_more                | Command submenu indicator                         |
+# | disp_sidebar_fmt              | Format string for default sidebar                 |
+# | disp_sidebar_width            | Max width for sidebar                             |
+# | disp_sidebar_height           | Max height for sidebar                            |
+# | disp_splitwin_pct             | Percent of window for secondary sheet             |
+# | disp_graph_tick_x             | Character for graph x-axis ticks                  |
+# | disp_graph_reflines_x_charset | Charset for vertical reference lines on graph     |
+# | disp_graph_reflines_y_charset | Charset for horizontal reference lines on graph   |
+# | disp_ambig_width              | Display width for ambiguous width characters      |
+# | disp_note_none                | Visible contents of a cell whose value is None    |
+# | disp_truncator                | Indicator that contents are partially visible     |
+# | disp_oddspace                 | Displayable character for odd whitespace          |
+# | disp_note_getexc              | Cell note for exception during computation        |
+# | disp_note_typeexc             | Cell note for exception during type conversion    |
+# | disp_help_flags               | Help display flags                                |
+# | disp_formatter                | Formatter to create text in each cell             |
+# | disp_displayer                | Displayer to render text in each cell             |
+# | disp_histogram                | Histogram element character                       |
+# | disp_sparkline                | Characters to display sparkline                   |
+# | disp_scroll_context           | Minimum lines to keep visible when scrolling      |
+# | disp_canvas_charset           | Charset to render blocks on canvas                |
+# | disp_graph_pixel_random       | Randomly choose pixel attr instead of most common |
+# | disp_zoom_incr                | Amount to multiply zoomlevel when zooming         |
+# | disp_replay_play              | Status indicator for active replay                |
+# | disp_replay_record            | Status indicator for macro record                 |
+# | disp_edit_fill                | Edit field fill character                         |
+# | disp_unprintable              | Substitute character for unprintables             |
+# | disp_ibis_sidebar             | Which sidebar property to display                 |
+# | disp_wrap_break_long_words    | Break words longer than column width in multiline |
+# | disp_wrap_replace_whitespace  | Replace whitespace with spaces in multiline       |
+# | disp_wrap_placeholder         | Multiline string to indicate truncation           |
+# | disp_cmdpal_max               | Max number of suggestions for command palette     |
+# | disp_selected_note            | Note for selected rows                            |
+# | disp_lstatus_max              | Maximum length of left status line                |
+# | disp_status_sep               | Separator between statuses                        |
+# | disp_rstatus_fmt              | Right-side status format string                   |
+#        component_separators = { left = '', right = '' },
+#        section_separators = { left = '', right = '' },
